b222c19871ad1411e90014f297cfd620fef22702
Vinicius de Oliveira Santos <56230387+vinicius-ssantos@users.noreply.github.com>
2022-11-10 09:30:15 -0300

    Iniciando a aplicação front-End

---
+++ src/views/login.js
@@ -0,0 +1,72 @@
+import React from "react";
+import Card from "../components/card.js";
+import FormGroup from "../components/form-group.js";
+
+class Login extends React.Component {
+  state = {
+    email: "",
+    senha: "",
+  };
+
+  entrar = () => {
+    console.log("Email:", this.state.email);
+    console.log("Senha:", this.state.senha);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div
+            className="col-md-6"
+            style={{ position: "relative", left: "300px" }}
+          >
+            <Card title="Login">
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="bs-component">
+                    <fieldset>
+                      <FormGroup label="Email: *" htmlFor="exampleInputEmail">
+                        <input
+                          type="email"
+                          value={this.state.email}
+                          onChange={(e) =>
+                            this.setState({ email: e.target.value })
+                          }
+                          className="form-control"
+                          id="exampleInputEmail"
+                          aria-describedby="emailHelp"
+                          placeholder="Digite o Email"
+                        />
+                      </FormGroup>
+                      <FormGroup
+                        label="Senha: *"
+                        htmlFor="exampleInputPassword1"
+                      >
+                        <input
+                          type="password"
+                          value={this.state.senha}
+                          onChange={(e) =>
+                            this.setState({ senha: e.target.value })
+                          }
+                          className="form-control"
+                          id="exampleInputPassword1"
+                          placeholder="Password"
+                        />
+                      </FormGroup>
+                      <button onClick={this.entrar} className="btn btn-success">
+                        Entrar
+                      </button>
+                      <button className="btn btn-danger">Cadastrar</button>
+                    </fieldset>
+                  </div>
+                </div>
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Login;
